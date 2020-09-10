ece7d0c5cbb96489acb10c7cf87ad7695036cb48
Captain Throwback <captainthrowback@hotmail.com>
2020-09-10 14:03:55 -0400

    time_daemon: remove unneeded init entries
    - Restarting service prevents vendor from being unmounted
      and is not needed as service remains running

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,7 @@ TW_THEME := portrait_hdpi
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 420
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
 TW_EXCLUDE_TWRPAPP := true
 TW_EXTRA_LANGUAGES := true
 TW_HAPTICS_TSPDRV := true
