553a6fb174fb6f1dc0e51b5eed0d92c984fe9f67
Captain Throwback <captainthrowback@hotmail.com>
2020-10-23 13:25:39 -0400

    prepdecrypt: don't set props on Nougat or older

--- recovery/root/system/bin/prepdecrypt.sh
+++ recovery/root/system/bin/prepdecrypt.sh
@@ -187,87 +187,93 @@ if [ "$sdkver" -lt 29 ]; then
 	fi
 fi
 
-is_fastboot_boot=$(grep skip_initramfs /proc/cmdline)
-if [ "$SETPATCH" = false ] || [ -n "$is_fastboot_boot" ]; then
-	# Be sure to increase the PLATFORM_VERSION in build/core/version_defaults.mk to override Google's anti-rollback features to something rather insane
-	update_default_values "$osver" "$osver_orig" "OS version" "ro.build.version.release" osver_default_value
-	update_default_values "$patchlevel" "$patchlevel_orig" "Security Patch Level" "ro.build.version.security_patch" patchlevel_default_value
-else
-	SETPATCH=true
-	BUILDPROP=build.prop
-	TEMPSYS=/s
-	suffix=$(getprop ro.boot.slot_suffix)
-	if [ -z "$suffix" ]; then
-		suf=$(getprop ro.boot.slot)
-		if [ -n "$suf" ]; then
-			suffix="_$suf"
+if [ "$sdkver" -ge 26 ]; then
+	is_fastboot_boot=$(grep skip_initramfs /proc/cmdline)
+	if [ "$SETPATCH" = false ] || [ -n "$is_fastboot_boot" ]; then
+		# Be sure to increase the PLATFORM_VERSION in build/core/version_defaults.mk to override Google's anti-rollback features to something rather insane
+		update_default_values "$osver" "$osver_orig" "OS version" "ro.build.version.release" osver_default_value
+		update_default_values "$patchlevel" "$patchlevel_orig" "Security Patch Level" "ro.build.version.security_patch" patchlevel_default_value
+	else
+		log_print 2 "Build tree is Oreo or above. Proceed with setting props..."
+		SETPATCH=true
+		BUILDPROP=build.prop
+		TEMPSYS=/s
+		suffix=$(getprop ro.boot.slot_suffix)
+		if [ -z "$suffix" ]; then
+			suf=$(getprop ro.boot.slot)
+			if [ -n "$suf" ]; then
+				suffix="_$suf"
+			fi
 		fi
-	fi
-	syspath="/dev/block/bootdevice/by-name/system$suffix"
+		syspath="/dev/block/bootdevice/by-name/system$suffix"
 
-	if [ "$sdkver" -ge 29 ]; then
-		MNT_VENDOR=true
-		TEMPVEN=/v
-		venpath="/dev/block/bootdevice/by-name/vendor$suffix"
+		if [ "$sdkver" -ge 29 ]; then
+			MNT_VENDOR=true
+			TEMPVEN=/v
+			venpath="/dev/block/bootdevice/by-name/vendor$suffix"
 
-		temp_mount "$TEMPVEN" "vendor" "$venpath"
+			temp_mount "$TEMPVEN" "vendor" "$venpath"
 
-		if [ -f "$TEMPVEN/$BUILDPROP" ]; then
-			log_print 2 "Vendor Build.prop exists! Reading vendor properties from build.prop..."
-			vensdkver=$(grep -i 'ro.vendor.build.version.sdk=' "$TEMPVEN/$BUILDPROP"  | cut -f2 -d'=' -s)
-			log_print 2 "Current vendor Android SDK version: $vensdkver"
-			if [ "$vensdkver" -gt 25 ]; then
-				log_print 2 "Current vendor is Oreo or above. Proceed with setting vendor security patch level..."
-				check_resetprop
-				log_print 2 "Current Vendor Security Patch Level: $venpatchlevel"
-				venpatchlevel=$(grep -i 'ro.vendor.build.security_patch=' "$TEMPVEN/$BUILDPROP"  | cut -f2 -d'=' -s)
-				if [ -n "$venpatchlevel" ]; then
-					$setprop_bin "ro.vendor.build.security_patch" "$venpatchlevel"
-					sed -i "s/\<ro.vendor.build.security_patch=\>.*/ro.vendor.build.security_patch=""$venpatchlevel""/g" "/$DEFAULTPROP" ;
-					log_print 2 "New Vendor Security Patch Level: $venpatchlevel"
+			if [ -f "$TEMPVEN/$BUILDPROP" ]; then
+				log_print 2 "Vendor Build.prop exists! Reading vendor properties from build.prop..."
+				vensdkver=$(grep -i 'ro.vendor.build.version.sdk=' "$TEMPVEN/$BUILDPROP"  | cut -f2 -d'=' -s)
+				log_print 2 "Current vendor Android SDK version: $vensdkver"
+				if [ "$vensdkver" -gt 25 ]; then
+					log_print 2 "Current vendor is Oreo or above. Proceed with setting vendor security patch level..."
+					check_resetprop
+					log_print 2 "Current Vendor Security Patch Level: $venpatchlevel"
+					venpatchlevel=$(grep -i 'ro.vendor.build.security_patch=' "$TEMPVEN/$BUILDPROP"  | cut -f2 -d'=' -s)
+					if [ -n "$venpatchlevel" ]; then
+						$setprop_bin "ro.vendor.build.security_patch" "$venpatchlevel"
+						sed -i "s/\<ro.vendor.build.security_patch=\>.*/ro.vendor.build.security_patch=""$venpatchlevel""/g" "/$DEFAULTPROP" ;
+						log_print 2 "New Vendor Security Patch Level: $venpatchlevel"
+					fi
+				else
+					log_print 2 "Current vendor is Nougat or older. Skipping vendor security patch level setting..."
 				fi
+			fi
+		fi
+
+		temp_mount "$TEMPSYS" "system" "$syspath"
+
+		sar=$(getprop ro.build.system_root_image)
+		if [ "$sar" = "true" ]; then
+			log_print 2 "System-as-Root device detected! Updating build.prop path variable..."
+			BUILDPROP="system/build.prop"
+			log_print 2 "Build.prop location set to $BUILDPROP."
+		fi
+		if [ -f "$TEMPSYS/$BUILDPROP" ]; then
+			log_print 2 "Build.prop exists! Reading system properties from build.prop..."
+			sdkver=$(grep -i 'ro.build.version.sdk=' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+			log_print 2 "Current system Android SDK version: $sdkver"
+			if [ "$sdkver" -gt 25 ]; then
+				log_print 2 "Current system is Oreo or above. Proceed with setting vendor security patch level..."
+				if [ -z "$setprop_bin" ]; then
+					check_resetprop
+				fi
+				# TODO: It may be better to try to read these from the boot image than from /system
+				log_print 2 "Current OS Version: $osver"
+				osver=$(grep -i 'ro.build.version.release=' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+				if [ -n "$osver" ]; then
+					$setprop_bin "ro.build.version.release" "$osver"
+					sed -i "s/\<ro.build.version.release=\>.*/ro.build.version.release=""$osver""/g" "/$DEFAULTPROP" ;
+					log_print 2 "New OS Version: $osver"
+				fi
+				log_print 2 "Current Security Patch Level: $patchlevel"
+				patchlevel=$(grep -i 'ro.build.version.security_patch=' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
+				if [ -n "$patchlevel" ]; then
+					$setprop_bin "ro.build.version.security_patch" "$patchlevel"
+					sed -i "s/\<ro.build.version.security_patch=\>.*/ro.build.version.security_patch=""$patchlevel""/g" "/$DEFAULTPROP" ;
+					log_print 2 "New Security Patch Level: $patchlevel"
+				fi
+				finish
 			else
 				log_print 2 "Current vendor is Nougat or older. Skipping vendor security patch level setting..."
+				finish
 			fi
 		fi
 	fi
-
-	temp_mount "$TEMPSYS" "system" "$syspath"
-
-	sar=$(getprop ro.build.system_root_image)
-	if [ "$sar" = "true" ]; then
-		log_print 2 "System-as-Root device detected! Updating build.prop path variable..."
-		BUILDPROP="system/build.prop"
-		log_print 2 "Build.prop location set to $BUILDPROP."
-	fi
-	if [ -f "$TEMPSYS/$BUILDPROP" ]; then
-		log_print 2 "Build.prop exists! Reading system properties from build.prop..."
-		sdkver=$(grep -i 'ro.build.version.sdk=' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-		log_print 2 "Current system Android SDK version: $sdkver"
-		if [ "$sdkver" -gt 25 ]; then
-			log_print 2 "Current system is Oreo or above. Proceed with setting OS version and security patch level..."
-			if [ -z "$setprop_bin" ]; then
-				check_resetprop
-			fi
-			# TODO: It may be better to try to read these from the boot image than from /system
-			log_print 2 "Current OS Version: $osver"
-			osver=$(grep -i 'ro.build.version.release=' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-			if [ -n "$osver" ]; then
-				$setprop_bin "ro.build.version.release" "$osver"
-				sed -i "s/\<ro.build.version.release=\>.*/ro.build.version.release=""$osver""/g" "/$DEFAULTPROP" ;
-				log_print 2 "New OS Version: $osver"
-			fi
-			log_print 2 "Current Security Patch Level: $patchlevel"
-			patchlevel=$(grep -i 'ro.build.version.security_patch=' "$TEMPSYS/$BUILDPROP"  | cut -f2 -d'=' -s)
-			if [ -n "$patchlevel" ]; then
-				$setprop_bin "ro.build.version.security_patch" "$patchlevel"
-				sed -i "s/\<ro.build.version.security_patch=\>.*/ro.build.version.security_patch=""$patchlevel""/g" "/$DEFAULTPROP" ;
-				log_print 2 "New Security Patch Level: $patchlevel"
-			fi
-			finish
-		else
-			log_print 2 "Current system is Nougat or older. Skipping OS version and security patch level setting..."
-			finish
-		fi
-	fi
+else
+	log_print 2 "Build tree is Nougat or older. Skip setting props."
+	finish
 fi
